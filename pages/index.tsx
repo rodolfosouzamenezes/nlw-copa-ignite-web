71cbe43c146f818ce3739d6f1a6342f90d123a5e
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-01 19:05:36 -0300

    feat: displayed pools count

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>Hello World</h1>
+    <h1>Contagem: {props.count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const responde = await fetch('http://localhost:3333/pools/count')
+  const data = await responde.json()
+
+  console.log(data);
+
+  return {
+    props: {
+      count: data.count,
+    },
+  }
 }
